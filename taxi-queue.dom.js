8d06e0ff4891c95491a219f666ea92a66a67b7bd
motsamaiteboho <96079592+motsamaiteboho@users.noreply.github.com>
2022-05-30 13:50:26 +0200

    what i have do

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -2,10 +2,31 @@
 
 
 // DOM element references
+const passeagerCount = document.querySelector(".passenger_queue_count");
+const joinBtn = document.querySelector(".join_queue");
+const leaveBtn = document.querySelector(".leave_queue");
+
+const taxiCount = document.querySelector(".taxi_queue_count");
+const jointaxiBtn = document.querySelector(".join_taxi_queue");
+const departBtn = document.querySelector(".depart");
 
 // create Factory Function instance
 
-const taxiQueue = TaxiQueue();
+let current_passeager_count = 0;
+let current_taxi_count = 0;
+
+const storedPassegerCount = localStorage.getItem('passeager_count');
+if(storedPassegerCount){
+    current_passeager_count = (storedPassegerCount);
+}
+const storedTaxiCount = localStorage.getItem('taxi_count');
+if(storedTaxiCount){
+    current_taxi_count = (storedTaxiCount);
+}
+const taxiQueue = TaxiQueue(current_passeager_count, current_taxi_count);
+
+passeagerCount.innerHTML = taxiQueue.queueLength();
+taxiCount.innerHTML = taxiQueue.taxiQueueLength();
 
 // DOM events
 
