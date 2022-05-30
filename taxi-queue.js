b0ddbe786ad76ead10c8df2bafdeaf9b11e263d3
motsamaiteboho <96079592+motsamaiteboho@users.noreply.github.com>
2022-05-30 13:28:11 +0200

    completed the factory faction unit testing

--- taxi-queue.js
+++ taxi-queue.js
@@ -1,28 +1,37 @@
 function TaxiQueue() {
 
+	var counter = 0;
+	var taxiCounter = 0;
 
 	function joinQueue() {
-
+		counter++;
 	}
 
 	function leaveQueue() {
-
+		if(counter > 0)
+		{
+			counter--;
+		}
 	}
 
 	function joinTaxiQueue() {
-
+		taxiCounter++;
 	}
 
-	function queueLength() {
-
+	function queueLength(){
+		return counter;
 	}
 
 	function taxiQueueLength() {
-
+		return taxiCounter;
 	}
 
 	function taxiDepart(){
-
+		if(counter > 12 && taxiCounter > 0)
+		{
+			taxiCounter--;
+			counter = counter - 12;
+		}
 	}
 
 	return {
